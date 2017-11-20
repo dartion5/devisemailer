45f1e0d00fe867d2b3d8fd5957ee4a36cf3aafcf
Darshan <dartion@gmail.com>
2017-11-20 22:22:33 +0000

    Adding SASS style gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
+  root to: 'pages#index'
+
+  get 'hello', to: 'pages#contact'
+
+  get 'pages/about'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-  root to: 'pages#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
